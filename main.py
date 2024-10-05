64ed54530b7d6b43a3edf466d517df74d46078ad
Jonas200909 <yonitasamd@hotmail.com>
2024-10-05 16:52:52 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+traduccion{
+    "JUICIOSO":"alguien que es responsable"
+    "AGUACATE":"conocido tambien como palta"
+    "CANGUIL":"maiz pira o palomitas de maiz"
+
+}
+palabra= input("escribe una palabra en mayuscula para buscar el significado:")
+
+if palabra in traduccion.keys():
+    print("su significado es:", traduccion,vaules)
+else:
+    print("la palabra no existe")
